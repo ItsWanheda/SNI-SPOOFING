4f54ba26bc0662e52a8bf0ae06a0589a2b0c1753
Wanheda7737 <Wanheda.work@gmail.com>
2026-06-10 20:02:13 +0200

    feat: add base connection monitoring class

--- Log_Monitoring.py
+++ Log_Monitoring.py
@@ -1,17 +1,16 @@
-import socket
+# Log_Monitoring.py
+import uuid
 import threading
 
-
 class MonitorConnection:
-    def __init__(self, sock: socket.socket, src_ip, dst_ip,
-                 src_port, dst_port):
-        self.monitor = True
-        self.syn_seq = -1
-        self.syn_ack_seq = -1
+    def __init__(self, sock, src_ip, dst_ip, src_port, dst_port):
+        self.id = (src_ip, src_port, dst_ip, dst_port)
+        self.sock = sock
         self.src_ip = src_ip
         self.dst_ip = dst_ip
         self.src_port = src_port
         self.dst_port = dst_port
-        self.id = (self.src_ip, self.src_port, self.dst_ip, self.dst_port)
+        self.syn_seq = -1
+        self.syn_ack_seq = -1
+        self.monitor = True
         self.thread_lock = threading.Lock()
-        self.sock = sock

c7a8c8d6a08cdfa6cf6b295974876605419e7abc
Wanheda7737 <Wanheda.work@gmail.com>
2026-06-10 20:02:45 +0200

    feat: add base TcpInjector class using pydivert

--- Injector.py
+++ Injector.py
@@ -1,37 +1,25 @@
-import sys
-from abc import ABC, abstractmethod
+# Injector.py
+import threading
+from pydivert import PyDivert
 
-from pydivert import WinDivert, Packet
+class TcpInjector:
+    def __init__(self, w_filter):
+        self.w = PyDivert(w_filter)
+        self.running = True
+        self.thread = threading.Thread(target=self._run, daemon=True)
+        self.thread.start()
 
+    def _run(self):
+        while self.running:
+            try:
+                packet = self.w.recv()
+                self.inject(packet)
+            except Exception as e:
+                if self.running:
+                    print(f"Injector error: {e}")
 
-# from pydivert.consts import *
-
-
-class TcpInjector(ABC):
-    def __init__(self, w_filter: str):
-        # self.interface_ipv4 = interface_ipv4
-        # self.interface_ipv6 = interface_ipv6
-        # ip_filter = ip4_filter = ip6_filter = ""
-        # if self.interface_ipv4:
-        #     ip4_filter = "(ip.SrcAddr == " + self.interface_ipv4 + " or ip.DstAddr == " + self.interface_ipv4 + ")"
-        #     ip_filter = ip4_filter
-        # if self.interface_ipv6:
-        #     ip6_filter = "(ipv6.SrcAddr == " + self.interface_ipv6 + " or ipv6.DstAddr == " + self.interface_ipv6 + ")"
-        #     ip_filter = ip6_filter
-        # if self.interface_ipv4 and self.interface_ipv6:
-        #     ip_filter = "(" + ip4_filter + " or " + ip6_filter + ")"
-        #
-        # self.filter = "tcp"
-        # if ip_filter:
-        #     self.filter += " and " + ip_filter
-        self.w: WinDivert = WinDivert(w_filter)
-
-    @abstractmethod
-    def inject(self, packet: Packet):
-        sys.exit("Not implemented")
+    def inject(self, packet):
+        raise NotImplementedError
 
     def run(self):
-        with self.w:
-            while True:
-                packet = self.w.recv(65575)
-                self.inject(packet)
+        self.thread.join()
